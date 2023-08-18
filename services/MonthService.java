c03fa6c192661f76447153d5e68172bf64775819
Shabadam <shabadamalsu@gmail.com>
2023-08-18 22:42:57 +0500

    Initial commit

---
+++ services/MonthService.java
@@ -0,0 +1,17 @@
+package ru.netology.javaqa.javaqacircle.services;
+
+public class MonthService {
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0;
+        int money = 0;
+        for (int month = 0; month < 12; month++) {
+            if (money <= threshold) {
+                money = money + income - expenses;
+            } else {
+                money = (money - expenses) / 3;
+                count++;
+            }
+        }
+        return count;
+    }
+}
